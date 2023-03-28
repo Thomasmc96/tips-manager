7051e4682e60d3ca5141f46b1820af38dcfbfb42
Thom9521 <thomas96mc@gmail.com>
2023-03-28 08:27:03 +0200

    typo

--- src/components/tipsBuilder/Summary.js
+++ src/components/tipsBuilder/Summary.js
@@ -5,7 +5,7 @@ const Summary = () => {
         <div className="container mx-auto mt-2">
             <h1 className="text-center text-3xl">Så du med i kampen! ⚽</h1>
             <p className="text-center">...næsten!</p>
-            <p>Husk at overfør <span>100 kr.</span> til <span>30 32 12 12</span></p>
+            <p>Husk at overføre <span>100 kr.</span> til <span>30 32 12 12</span></p>
         </div>
     )
 }
