724fff65dda1621b2f10d8dea888624d4a74ce88
Thom9521 <Thomas96mc@gmail.com>
2023-03-29 19:43:21 +0200

    table reversed

--- src/components/standings/Table.js
+++ src/components/standings/Table.js
@@ -8,17 +8,51 @@ const Table = ({ coupons }) => {
       <div className="overflow-x-auto sm:rounded-lg">
         <table className="table-auto overflow-x-auto w-full">
           <thead>
-            <tr className="bg-darkGreen">
+            {/* <tr className="bg-darkGreen">
               <th className="text-left px-2">Navn</th>
               {matchesJson.map(({ id, name, participants }) => (
                 <th className="p-2">
                   {participants[0].name}-{participants[1].name}
                 </th>
               ))}
+            </tr> */}
+            <tr className="bg-darkGreen">
+              <th>Kampe</th>
+              {coupons.map(({ name }) => (
+                <th>{name}</th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            {coupons.map(({ name, predictions }, i) => (
+            {matchesJson.map(({ id, name, participants }, i) => (
+              <tr>
+                <td
+                  className={`p-6 text-center ${
+                    i % 2 === 0 ? "bg-lightGreen" : "bg-transparent"
+                  }`}
+                >
+                  {participants[0].name}-{participants[1].name}
+                </td>
+                {coupons.map(({ predictions }) => (
+                  <>
+                    {JSON.parse(predictions).map((prediction) => {
+                      if (prediction.id === id) {
+                        return (
+                          <td
+                            className={`p-6 text-center ${
+                              i % 2 === 0 ? "bg-lightGreen" : "bg-transparent"
+                            }`}
+                          >
+                            {prediction.prediction}
+                          </td>
+                        );
+                      }
+                    })}
+                  </>
+                ))}
+              </tr>
+            ))}
+            {/* {coupons.map(({ name, predictions }, i) => (
               <tr
                 className={`p-6 ${
                   i % 2 === 0 ? "bg-lightGreen" : "bg-transparent"
@@ -29,7 +63,7 @@ const Table = ({ coupons }) => {
                   <td className="text-center">{prediction}</td>
                 ))}
               </tr>
-            ))}
+            ))} */}
           </tbody>
         </table>
       </div>
