d9e109396d1d4e404a5a7b8a676ae1f9a61f72af
Thomas <tns@hiper.dk>
2024-02-05 16:40:22 +0100

    email styles wip

--- src/components/admin/Email.js
+++ src/components/admin/Email.js
@@ -15,81 +15,85 @@ const Email = () => {
 
 const EmailStandingsForm = () => {
 
-    const [emailText,setEmailText] = useState('');
+    const [emailText, setEmailText] = useState('');
     const [isCustom, setIsCustom] = useState(false);
     const [loading, setLoading] = useState(false);
 
     const sendMail = (e) => {
         e.preventDefault();
 
-    axios
-      .post(`${environment[0]}/server/endpoints/email/sendStandings.php`, {
-        message: isCustom ? emailText : ''
-      })
-      .then((response) => {
-        if (response.data.code === 200) {
-          console.log(response)
-        } else {
-            console.log(response);
-          alert("Der skete desværre en fejl. Prøv igen.");
-        }
-      })
-      .catch((error) => {
-        alert("Der skete desværre en fejl. Prøv igen.");
-        console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+        axios
+            .post(`${environment[0]}/server/endpoints/email/sendStandings.php`, {
+                message: isCustom ? emailText : ''
+            })
+            .then((response) => {
+                if (response.data.code === 200) {
+                    console.log(response)
+                } else {
+                    console.log(response);
+                    alert("Der skete desværre en fejl. Prøv igen.");
+                }
+            })
+            .catch((error) => {
+                alert("Der skete desværre en fejl. Prøv igen.");
+                console.log(error);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
 
     }
 
     return (
-        <div className="">
-            <h2 className="text-2xl mb-3 mt-5">Send mail med stillingen</h2>
-            <div className="flex items-center mt-2 w-full mb-2">
-                <input
-                    id="isCustomCheckbox"
-                    type="checkbox"
-                    value={isCustom}
-                    onClick={() => {
-                        setIsCustom(!isCustom);
-                    }}
-                    className="w-8 h-8 rounded"
-                />
-                <label htmlFor="isCustomCheckbox" className="ml-2 text-md font-medium">
-                    Skriv selv en besked
-                </label>
-            </div>
-            <form className="container mt-2" onSubmit={sendMail}>
-                {isCustom && 
-                    <textarea 
-                        className="text-black w-full p-2 rounded-md h-48"
-                        type="textarea"
-                        placeholder="Her er nuværende stilling for EM 2024 tipskonkurrencen."
-                        value={emailText}
-                        onChange={(e) => setEmailText(e.target.value)}
+        <div className="container emailContainer">
+            <h2>Send <span className="yellowText">mails</span> til alle med stillingen</h2>
+
+            <form className="emailForm" onSubmit={sendMail}>
+                <div className="checkboxContainer">
+                    <input
+                        id="isCustomCheckbox"
+                        type="checkbox"
+                        value={isCustom}
+                        onClick={() => {
+                            setIsCustom(!isCustom);
+                        }}
+                        className="w-8 h-8 rounded"
                     />
+                    <label className="checkboxLabel" htmlFor="isCustomCheckbox">
+                        Jeg vil selv skrive en besked
+                    </label>
+                </div>
+                {isCustom &&
+                    <>
+                        <label>Min besked</label>
+                        <textarea
+                            className="text-black w-full p-2 rounded-md h-48"
+                            type="textarea"
+                            placeholder="Her er nuværende stilling for EM 2024 tipskonkurrencen."
+                            value={emailText}
+                            onChange={(e) => setEmailText(e.target.value)}
+                        />
+                    </>
                 }
-                <button 
+                <button
                     type="submit"
-                    className="bg-sandBeige rounded-md sm:w-80 w-full h-10 text-black text-lg hover:cursor-pointer hover:scale-105 duration-200 mb-7 flex justify-center items-center"
-                    >
+                    className="submitBtn"
+                >
                     {!loading ? (
                         <>Send</>
                     ) : (
                         <FidgetSpinner
-                        visible={true}
-                        height="30"
-                        width="30"
-                        ariaLabel="dna-loading"
-                        wrapperStyle={{}}
-                        wrapperClass="dna-wrapper"
-                        ballColors={["#003e21", "#067242", "#098b54"]}
-                        backgroundColor="#f8d098"
+                            visible={true}
+                            height="30"
+                            width="30"
+                            ariaLabel="dna-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="dna-wrapper"
+                            ballColors={["#003e21", "#067242", "#098b54"]}
+                            backgroundColor="#f8d098"
                         />
                     )}
-                    </button>
+                </button>
             </form>
         </div>
     )
