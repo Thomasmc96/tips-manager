762ef42c3efef7fdce746e6d103739715c10e9ce
Thomas <tns@hiper.dk>
2024-02-22 16:06:09 +0100

    minor

--- src/components/utils/Loader.js
+++ src/components/utils/Loader.js
@@ -1,12 +1,12 @@
 import './Loader.css';
-import loaderBall from '../../assets/graphic/football.svg'
+import ball from '../../assets/graphic/football.svg'
 
 const Loader = () => {
     return (
         <div id="ajaxloader">
             <div className="outer"></div>
             <div className="soccer">
-                <img src={loaderBall} alt="loading..." />
+                <img src={ball} alt="loading..." />
             </div>
         </div>
     )
