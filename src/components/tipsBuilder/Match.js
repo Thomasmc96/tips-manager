e36149a685b8b62e0b6a9bae1eabcf9959819fc8
Thomas <tns@hiper.dk>
2024-02-22 09:02:41 +0100

    tipsbuilder style

--- src/components/tipsBuilder/Match.js
+++ src/components/tipsBuilder/Match.js
@@ -26,81 +26,94 @@ const Match = ({
   };
 
   return (
-    <div className="matchBackground">
-      <h3 className="text-xl text-center">
-        <div className="flex space-x-5 justify-center">
-          <div className="flex items-center space-x-3">
-            <img
-              src={`https://flags.tv2a.dk/tv2football/${match.home_team}.svg`}
-              alt={match.home_team}
-              className="w-12"
-            />
-            <p>{countryName(match.home_team)}</p>
+    <div className="match">
+      <img
+        src={`https://flags.tv2a.dk/tv2football/${match.home_team}.svg`}
+        alt={match.home_team}
+        className="flagHome"
+      />
+      <div>
+
+        <h3 className="text-xl text-center">
+          <div className="flex space-x-5 justify-center">
+            <div className="flex items-center space-x-3">
+              <img
+                src={`https://flags.tv2a.dk/tv2football/${match.home_team}.svg`}
+                alt={match.home_team}
+                className="flagHomeMobile"
+              />
+              <p>{countryName(match.home_team)}</p>
+            </div>
+            <span>-</span>
+            <div className="flex items-center space-x-3">
+              <p>{countryName(match.away_team)}</p>
+              <img
+                src={`https://flags.tv2a.dk/tv2football/${match.away_team}.svg`}
+                alt={match.away_team}
+                className="flagAwayMobile"
+              />
+            </div>
           </div>
-          <span>-</span>
-          <div className="flex items-center space-x-3">
-            <p>{countryName(match.away_team)}</p>
-            <img
-              src={`https://flags.tv2a.dk/tv2football/${match.away_team}.svg`}
-              alt={match.away_team}
-              className="w-12"
+        </h3>
+        <p className="text-center">
+          {getDateString(match.kickoff_dtm)}
+        </p>
+        <div className="flex space-x-3 justify-center pt-5">
+          <div>
+            <input
+              type="radio"
+              name={match.matches2_id + "_radio"}
+              id={`${match.matches2_id}_radio_1`}
+              className="hidden peer"
+              value={"1"}
+              onInput={(e) => handlePrediction(e, match.matches2_id)}
             />
+            <label
+              htmlFor={`${match.matches2_id}_radio_1`}
+              className="w-20 h-10 flex justify-center items-center rounded-md bg-normalGreen hover:cursor-pointer hover:scale-110 duration-200 peer-checked:bg-darkGreen peer-checked:text-gold peer-checked:border-gold peer-checked:border-[1px]"
+            >
+              1
+            </label>
           </div>
-        </div>
-      </h3>
-      <p className="text-center">
-        {getDateString(match.kickoff_dtm)}
-      </p>
-      <div className="flex space-x-3 justify-center pt-5">
-        <div>
-          <input
-            type="radio"
-            name={match.matches2_id + "_radio"}
-            id={`${match.matches2_id}_radio_1`}
-            className="hidden peer"
-            value={"1"}
-            onInput={(e) => handlePrediction(e, match.matches2_id)}
-          />
-          <label
-            htmlFor={`${match.matches2_id}_radio_1`}
-            className="w-20 h-10 flex justify-center items-center rounded-md bg-normalGreen hover:cursor-pointer hover:scale-110 duration-200 peer-checked:bg-darkGreen"
-          >
-            1
-          </label>
-        </div>
-        <div>
-          <input
-            type="radio"
-            name={match.matches2_id + "_radio"}
-            id={`${match.matches2_id}_radio_x`}
-            className="hidden peer"
-            value={"x"}
-            onInput={(e) => handlePrediction(e, match.matches2_id)}
-          />
-          <label
-            htmlFor={`${match.matches2_id}_radio_x`}
-            className="w-20 h-10 flex justify-center items-center rounded-md bg-normalGreen hover:cursor-pointer hover:scale-110 duration-200 peer-checked:bg-darkGreen"
-          >
-            x
-          </label>
-        </div>
-        <div>
-          <input
-            type="radio"
-            name={`${match.matches2_id}_radio`}
-            id={`${match.matches2_id}_radio_2`}
-            className="hidden peer"
-            value={"2"}
-            onInput={(e) => handlePrediction(e, match.matches2_id)}
-          />
-          <label
-            htmlFor={`${match.matches2_id}_radio_2`}
-            className="w-20 h-10 flex justify-center items-center rounded-md bg-normalGreen hover:cursor-pointer hover:scale-110 duration-200 peer-checked:bg-darkGreen"
-          >
-            2
-          </label>
+          <div>
+            <input
+              type="radio"
+              name={match.matches2_id + "_radio"}
+              id={`${match.matches2_id}_radio_x`}
+              className="hidden peer"
+              value={"x"}
+              onInput={(e) => handlePrediction(e, match.matches2_id)}
+            />
+            <label
+              htmlFor={`${match.matches2_id}_radio_x`}
+              className="w-20 h-10 flex justify-center items-center rounded-md bg-normalGreen hover:cursor-pointer hover:scale-110 duration-200 peer-checked:bg-darkGreen peer-checked:text-gold peer-checked:border-gold peer-checked:border-[1px]"
+            >
+              x
+            </label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name={`${match.matches2_id}_radio`}
+              id={`${match.matches2_id}_radio_2`}
+              className="hidden peer"
+              value={"2"}
+              onInput={(e) => handlePrediction(e, match.matches2_id)}
+            />
+            <label
+              htmlFor={`${match.matches2_id}_radio_2`}
+              className="w-20 h-10 flex justify-center items-center rounded-md bg-normalGreen hover:cursor-pointer hover:scale-110 duration-200 peer-checked:bg-darkGreen peer-checked:text-gold peer-checked:border-gold peer-checked:border-[1px]"
+            >
+              2
+            </label>
+          </div>
         </div>
       </div>
+      <img
+        src={`https://flags.tv2a.dk/tv2football/${match.away_team}.svg`}
+        alt={match.away_team}
+        className="flagAway"
+      />
     </div>
   );
 };
