35abd39e43fc3be26ddc333d5a9b9462744a0605
Thomasmc96 <tns@hiper.dk>
2024-03-02 22:24:14 +0100

    secure prize page

--- src/components/prize/Prize.js
+++ src/components/prize/Prize.js
@@ -5,12 +5,40 @@ import { getPodium } from "../Utils";
 import PodiumPart from "./PodiumPart";
 import Countdown from "react-countdown";
 import { verify } from "../Utils";
-import './Prize.css'
+import "./Prize.css";
 import Loader from "../utils/Loader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
 
 const Prize = () => {
-  const [loading, setLoading] = useState(true);
   const [authorized, setAuthorized] = useState();
+
+  useEffect(() => {
+    verify().then((response) => setAuthorized(response));
+  }, []);
+
+  const renderer = ({ completed }) => {
+    if (completed || authorized) {
+      return <Podium />;
+    } else {
+      return (
+        <h3 className="text-xl text-center mt-10 wait-text">
+          Se præmiepuljen og podium her når konkurrencen går i gang{" "}
+          <span className="trophyIcon">
+            <FontAwesomeIcon icon={faTrophy} />
+          </span>
+        </h3>
+      );
+    }
+  };
+
+  return (
+    <Countdown date={new Date("Jun 13, 2024 21:00:00")} renderer={renderer} />
+  );
+};
+
+const Podium = () => {
+  const [loading, setLoading] = useState(true);
   const [podium, setPodium] = useState({
     first: { names: [], totalPrize: 0, sharedPrize: 0 },
     second: { names: [], totalPrize: 0, sharedPrize: 0 },
@@ -24,7 +52,6 @@ const Prize = () => {
         if (response.data.code === 200) {
           setPodium(getPodium(response.data.coupons));
         }
-        verify().then((response) => setAuthorized(response));
       })
       .catch((error) => {
         console.log(error);
@@ -42,66 +69,51 @@ const Prize = () => {
     );
   }
 
-  const renderer = ({completed }) => {
-    if (completed || authorized) {
-      return (
-        <div className="prize container mx-auto flex flex-col items-center min-h-[80vh]">
-          <h1><span className="yellowText">Præmie</span>fordeling</h1>
-          <div className="flex flex-col justify-around flex-wrap sm:flex-row w-full mx-auto gap-14 sm:gap-0 mt-10">
-            <PodiumPart placement={podium.first} number={"1st"} />
-            {podium.second.names.length > 0 && (
-              <PodiumPart placement={podium.second} number={"2nd"} />
-            )}
-            {podium.third.names.length > 0 && (
-              <PodiumPart placement={podium.third} number={"3rd"} />
-            )}
-          </div>
-            <PrizeRules podium={podium} />
-        </div>
-      );
-    } else {
-      return (
-        <h3 className="text-xl text-center mt-10">
-          Se præmiepuljen og podium her når konkurrence går i gang 💰
-        </h3>
-      );
-    }
-  };
-
   return (
-    <Countdown date={new Date("Jun 13, 2024 21:00:00")} renderer={renderer} />
+    <div className="prize container mx-auto flex flex-col items-center min-h-[80vh]">
+      <h1>
+        <span className="yellowText">Præmie</span>fordeling
+      </h1>
+      <div className="flex flex-col justify-around flex-wrap sm:flex-row w-full mx-auto gap-14 sm:gap-0 mt-10">
+        <PodiumPart placement={podium.first} number={"1st"} />
+        {podium.second.names.length > 0 && (
+          <PodiumPart placement={podium.second} number={"2nd"} />
+        )}
+        {podium.third.names.length > 0 && (
+          <PodiumPart placement={podium.third} number={"3rd"} />
+        )}
+      </div>
+      <PrizeRules podium={podium} />
+    </div>
   );
 };
 
-
-const PrizeRules = ({podium}) => {
+const PrizeRules = ({ podium }) => {
   return (
-    <div className='rules'>
-    <h4>
-      Hvordan udregnes <span className="yellowText">præmien</span>?
-    </h4>
-    <ul>
-      <li>
-        Puljen består af{" "}
-        {podium.first.totalPrize +
-          podium.second.totalPrize +
-          podium.third.totalPrize}{" "}
-        kr.
-      </li>
-      <li>
-        Førstepladsen tager 70 %, andenpladsen tager 20 % og tredjepladsen
-        tager 10 %.
-      </li>
-      <li>
-        Er der to personer på førstepladsen, så deler de to både første-
-        og andenpræmien.
-      </li>
-      <li>
-        Er der tre personer på førstepladsen, så deler de hele puljen.
-      </li>
-    </ul>
-  </div>
-  )
-}
+    <div className="rules">
+      <h4>
+        Hvordan udregnes <span className="yellowText">præmien</span>?
+      </h4>
+      <ul>
+        <li>
+          Puljen består af{" "}
+          {podium.first.totalPrize +
+            podium.second.totalPrize +
+            podium.third.totalPrize}{" "}
+          kr.
+        </li>
+        <li>
+          Førstepladsen tager 70 %, andenpladsen tager 20 % og tredjepladsen
+          tager 10 %.
+        </li>
+        <li>
+          Er der to personer på førstepladsen, så deler de to både første- og
+          andenpræmien.
+        </li>
+        <li>Er der tre personer på førstepladsen, så deler de hele puljen.</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Prize;
