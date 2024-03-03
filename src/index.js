30edecfcbfa4734590c1f1c3d42fe7e6954244ca
Thomasmc96 <tns@hiper.dk>
2024-03-03 18:44:03 +0100

    serviceworker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <App />
-);
+root.render(<App />);
+serviceWorkerRegistration.register();
