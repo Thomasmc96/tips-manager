aed9df9fb972b1528cf1462f506b282ab4b9c754
Thomas <tns@hiper.dk>
2024-03-07 16:08:56 +0100

    search for name in tips-table

--- src/components/standings/Table.js
+++ src/components/standings/Table.js
@@ -15,24 +15,23 @@ const Table = () => {
   const [error, setError] = useState(false);
   const [coupons, setCoupons] = useState([]);
 
-
   var isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
 
   useEffect(() => {
     axios
-    .get(`${environment[0]}/server/endpoints/coupon/getResults.php`)
-    .then((response) => {
-      if (response.data.code === 200) {
-        setCoupons(sortByWins(response.data.coupons));
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .finally(() => {
-      setLoading(false);
-    });
-  
+      .get(`${environment[0]}/server/endpoints/coupon/getResults.php`)
+      .then((response) => {
+        if (response.data.code === 200) {
+          setCoupons(sortByWins(response.data.coupons));
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+
     axios
       .get(`${environment[0]}/server/endpoints/matches2/getAll.php`)
       .then((response) => {
@@ -55,15 +54,31 @@ const Table = () => {
 
   const isFinished = (match) => {
     if (match.home_team_goals !== null && match.away_team_goals !== null) {
-      return true
+      return true;
     }
     return false;
-  }
+  };
+
+  const filterCoupons = (e) => {
+    let couponsCopy = coupons;
+    const filterText = e.target.value.toUpperCase();
+
+    for (let i = 0; i < couponsCopy.length; i++) {
+      let coupon = couponsCopy[i];
+      if (coupon.name.toUpperCase().includes(filterText)) {
+        coupon.isHidden = false;
+      } else {
+        coupon.isHidden = true;
+      }
+    }
+
+    setCoupons(couponsCopy);
+  };
 
   if (loading || matchesLoading) {
     return (
       <div className="flex mx-auto justify-center h-40 items-center">
-        <Loader/>
+        <Loader />
       </div>
     );
   }
@@ -94,25 +109,49 @@ const Table = () => {
           >
             +
           </button>
+          <input
+            type="text"
+            placeholder="Søg efter navn"
+            className="rounded-sm ml-2 pl-1 text-black"
+            id="filterInput"
+            // onKeyUp={filterCoupons}
+            onChange={filterCoupons}
+            autoComplete="off"
+          />
         </section>
       )}
 
       <div className="mt-1 overflow-x-auto">
-        <table className="table-fixed relative border-[2px] border-solid border-white rounded-lg " style={{ zoom: zoom + "%" }}>
+        <table
+          className="table-fixed relative border-[2px] border-solid border-white rounded-lg "
+          style={{ zoom: zoom + "%" }}
+          id="tipsTable"
+        >
           <thead className="sticky top-0 z-50">
             <tr className="bg-darkestGreenNew border-[1px] border-white ">
               <th className="min-w-[12rem]"></th>
-              {coupons.map(({ coupons_id, name, amountCorrect }) => (
-                <th
-                  key={coupons_id}
-                  className="border-[1px] border-white w-40 px-1 text-xs"
-                >
-                  <div className="flex flex-col h-auto w-full justify-between py-1 px-2">
-                    <span className="couponName">{name}</span>
-                    <span className="mt-1">{amountCorrect}<FontAwesomeIcon className="ml-1" icon={faCheck}></FontAwesomeIcon></span>
-                  </div>
-                </th>
-              ))}
+              {coupons.map(({ coupons_id, name, amountCorrect, isHidden }) => {
+                if (isHidden) {
+                  return "";
+                }
+                return (
+                  <th
+                    key={coupons_id}
+                    className="border-[1px] border-white w-40 px-1 text-xs"
+                  >
+                    <div className="flex flex-col h-auto w-full justify-between py-1 px-2">
+                      <span className="couponName">{name}</span>
+                      <span className="mt-1">
+                        {amountCorrect}
+                        <FontAwesomeIcon
+                          className="ml-1"
+                          icon={faCheck}
+                        ></FontAwesomeIcon>
+                      </span>
+                    </div>
+                  </th>
+                );
+              })}
             </tr>
           </thead>
           <tbody>
@@ -121,20 +160,19 @@ const Table = () => {
                 <td className={`p-2 text-center border-[1px] border-white`}>
                   <div className="flex flex-col">
                     <span className="countryNames">
-                      {countryName(match.home_team)} - {countryName(match.away_team)}
+                      {countryName(match.home_team)} -{" "}
+                      {countryName(match.away_team)}
                     </span>
                     <span className="yellowText matchInfo">
                       {!isFinished(match)
                         ? getDateString(match.kickoff_dtm)
-                        : match.home_team_goals +
-                        " - " +
-                        match.away_team_goals}
+                        : match.home_team_goals + " - " + match.away_team_goals}
                     </span>
                   </div>
                 </td>
-                {coupons.map(({ coupons_id, predictions }, j) =>
+                {coupons.map(({ coupons_id, predictions, isHidden }, j) =>
                   predictions.map((prediction, j) => {
-                    if (prediction.id === match.matches2_id) {
+                    if (prediction.id === match.matches2_id && !isHidden) {
                       return (
                         <td
                           key={coupons_id}
@@ -143,8 +181,8 @@ const Table = () => {
                             (prediction.won && isFinished(match)
                               ? "bg-lightGreenNew"
                               : !prediction.won && isFinished(match)
-                                ? "bg-red"
-                                : "")
+                              ? "bg-red"
+                              : "")
                           }
                         >
                           <span className="prediction" key={prediction.id}>
@@ -153,7 +191,7 @@ const Table = () => {
                         </td>
                       );
                     }
-                    return ''
+                    return "";
                   })
                 )}
               </tr>
